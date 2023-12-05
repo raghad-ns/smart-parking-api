f602cc8dd588d5a18f699a87c008e1f48ca53432
Mohammad-taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2023-12-05 10:28:30 +0200

    modify the Reflect entity

--- DB/Entities/Reflect.ts
+++ DB/Entities/Reflect.ts
@@ -1,5 +1,6 @@
-import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn, Relation } from "typeorm";
+import { BaseEntity, BeforeInsert, Column, Entity, OneToMany, PrimaryGeneratedColumn, Relation } from "typeorm";
 import { Transaction } from "./Transaction";
+import bcrypt from 'bcrypt'
 
 @Entity()
 export class Reflect extends BaseEntity {
@@ -11,6 +12,18 @@ export class Reflect extends BaseEntity {
 
   @Column('int', {nullable: true})
   amount: number;
+
+  @BeforeInsert()
+  async hashPassword() {
+    if (this.password) {
+      this.password = await bcrypt.hash(this.password, 10);
+    }
+  }
+  @Column('text')
+  password: string;
+
+  @Column('text')
+  mobileNo: string;
 
   @OneToMany(() => Transaction, (transaction) => transaction.source)
   transactions: Relation<Transaction[]>;
