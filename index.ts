a0ae8d57ca21d5ca2b9a096b46e51b507928bcd8
Mohammad-taradeh <98043145+Mohammad-taradeh@users.noreply.github.com>
2024-01-02 13:50:03 +0200

    update the connection to https

--- index.ts
+++ index.ts
@@ -5,12 +5,21 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser';
 import dataSource from './DB/dataSource';
 import home from './routes/car';
+import fs from 'fs'
+import https from 'https';
 //For env File 
 dotenv.config();
 
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+}
+
+const server = https.createServer(options, app);
 
 app.use(express.json());
 app.use(cors());
@@ -21,15 +30,16 @@ app.get('/', (req, res) => {
   res.send('Welcome to Express & TypeScript Server');
 });
 
-app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
-  dataSource.initialize()
-  .then(()=>{
-    console.log("DB is connected");
-    
-  })
-  .catch((err)=>{
-    console.log(err);
-  });
+server.listen(port, ()=>{
+  console.log(`App is lestining to PORT.. : ${port} using https`);
+
+  dataSource
+    .initialize()
+    .then(() => {
+      console.log("connected to database :)");
+    })
+    .catch((err) => {
+      console.log("failed to connect to db !! " + err);
+    });
   
 });
